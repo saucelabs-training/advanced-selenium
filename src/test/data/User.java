8e04683c10f3fa511750b6e8ffd9d3ff27ea542e
Titus Fortner <titusfortner@gmail.com>
2020-02-18 21:36:11 -0500

    implement default data for User

--- src/test/data/User.java
+++ src/test/data/User.java
@@ -1,8 +1,20 @@
 package test.data;
 
+import com.github.javafaker.Faker;
+
 public class User {
     private String username;
     private String password;
+    private static Faker faker;
+
+    public User() {
+        this.username = faker.name().username();
+        this.password = faker.internet().password();
+    }
+
+    public static User random() {
+        return new User();
+    }
 
     public static User valid() {
         User user = new User();
