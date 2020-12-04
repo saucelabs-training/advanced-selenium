575d275a667f9977f12f883468895e890e3e1920
titusfortner <titusfortner@gmail.com>
2020-12-04 11:40:30 -0600

    demonstrate fiels and scope in Java

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -4,19 +4,20 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class MyFirstTest {
+    public class MyFirstTest {
+        int answer;
 
-    @Test
-    public void canAddTwoPlusTwo() {
-        int answer = 2+2;
-        assertEquals("2+2=4", 4, answer);
+        @Test
+        public void canAddTwoPlusTwo() {
+            answer = 2+2;
+            assertEquals("2+2=4", 4, answer);
+        }
+
+        // Exercise: Write Test to subtract 2 from 2
+        @Test
+        public void canSubtractTwoFromTwo() {
+            answer = 2-2;
+            assertEquals("2-2=0", 0, answer);
+        }
     }
 
-    // Exercise: Write Test to subtract 2 from 2
-    @Test
-    public void canSubtractTwoFromTwo() {
-        int answer = 2-2;
-        assertEquals("2-2=0", 0, answer);
-    }
-
-}
