ad9a227211421d349b8858922c24af228328ea09
titusfortner <titusfortner@gmail.com>
2020-12-11 14:39:36 -0600

    Swag Labs Project

---
+++ src/test/java/projects/SwagLabsTest.java
@@ -0,0 +1,25 @@
+package test.java.projects;
+
+import org.junit.Test;
+import test.java.SauceTestBase;
+
+public class SwagLabsTest extends SauceTestBase {
+
+    @Test
+    public void purchaseItems() {
+        driver.get("https://www.saucedemo.com/");
+
+        // Sign  in with Standard User
+
+        // Add a Bolt T-Shirt and a Fleece Jacket to the Cart
+
+        // Go to Cart & Check out
+
+        // Submit Form with your information
+
+        // Finish Purchase
+
+        // Assert that the Pony Express Image is on the Page
+    }
+
+}
