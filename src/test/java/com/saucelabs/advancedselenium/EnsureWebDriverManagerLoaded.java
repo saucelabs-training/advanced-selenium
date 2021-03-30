360cdbcf20ae9839d282301690848d181926f4b8
titusfortner <titusfortner@gmail.com>
2021-03-30 12:16:47 -0500

    add webdrivermanager jars in lib

--- src/test/java/com/saucelabs/advancedselenium/EnsureWebDriverManagerLoaded.java
+++ src/test/java/com/saucelabs/advancedselenium/EnsureWebDriverManagerLoaded.java
@@ -1,10 +1,12 @@
 package test.java.com.saucelabs.advancedselenium;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class EnsureSeleniumLoaded {
+public class EnsureWebDriverManagerLoaded {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
         RemoteWebDriver driver = new ChromeDriver();
         driver.quit();
     }
