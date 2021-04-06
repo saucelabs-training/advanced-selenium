d94dd329ed8df544c2a0f45be0f8dabcccdda76a
titusfortner <titusfortner@gmail.com>
2021-04-06 18:03:19 -0500

    add reporting

--- src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/BaseTest.java
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/BaseTest.java
@@ -1,12 +1,19 @@
 package com.saucelabs.advancedselenium.saucedemo.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.TestWatcher;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Optional;
 
 public class BaseTest {
     ChromeDriver driver = null;
+
+    @RegisterExtension
+    public MyTestWatcher myTestWatcher = new MyTestWatcher();
 
     @BeforeEach
     public void setUp() {
@@ -14,8 +21,33 @@ public class BaseTest {
         driver = new ChromeDriver();
     }
 
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
+    public class MyTestWatcher implements TestWatcher {
+        @Override
+        public void testFailed(ExtensionContext context, Throwable cause) {
+            try {
+                System.out.println("Test Failed!");
+            } catch (Exception ignored) {
+            } finally {
+                driver.quit();
+            }
+        }
+
+        @Override
+        public void testSuccessful(ExtensionContext context) {
+            try {
+                System.out.println("Test Passed!");
+            } catch (Exception ignored) {
+            } finally {
+                driver.quit();
+            }
+        }
+
+        @Override
+        public void testAborted(ExtensionContext context, Throwable cause) {
+        }
+
+        @Override
+        public void testDisabled(ExtensionContext context, Optional<String> reason) {
+        }
     }
 }
