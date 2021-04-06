94d812cd6dda12f6b5e0e140bd43a473b6a2b8ee
titusfortner <titusfortner@gmail.com>
2021-04-06 17:48:24 -0500

    define Journey

---
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/JourneyTest.java
@@ -0,0 +1,54 @@
+package com.saucelabs.advancedselenium.saucedemo.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class JourneyTest {
+
+    @Test
+    public void userJourney() {
+        // Go to Sauce Demo Site
+        // Attempt Sign in with locked user
+        // Validate error
+        // Sign in with Standard User
+        // Validate authenticated
+        // Log Out
+        // Validate unauthenticated
+        // Log In Again
+        // Open Bolt T-Shirt About Page
+        // Verify correct page
+        // Add a Bolt T-Shirt to the Cart
+        // Verify in cart
+        // Remove Item
+        // Verify not in cart
+        // Go back to Products page
+        // Verify correct page
+        // Add a Fleece Jacket, Bike Light & Backpack to the Cart
+        // Verify 3 items in cart
+        // Remove Bike Light
+        // Verify 2 items in art
+        // Go to Cart Page
+        // Verify Jacket & backpack are in cart
+        // Remove Backpack
+        // Verify backpack not in cart
+        // Back to Shopping
+        // Verify correct page
+        // Back to Cart
+        // Go to Checkout Page
+        // Verify correct page
+        // Cancel
+        // Verify correct page
+        // Go back to Checkout Page
+        // Select Continue
+        // Verify errors
+        // Enter info and continue
+        // Verify correct page
+        // Cancel
+        // Verify correct page
+        // Go to cart
+        // Go to checkout
+        // re-enter info
+        // Verify correct item
+        // Finish
+        // Verify success message
+    }
+}
