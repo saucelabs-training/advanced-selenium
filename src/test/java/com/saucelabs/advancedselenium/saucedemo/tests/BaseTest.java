8139b59616ac13fa7940ccab2f76446207d12b99
titusfortner <titusfortner@gmail.com>
2021-04-06 18:01:10 -0500

    implement Base Test class

---
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/BaseTest.java
@@ -0,0 +1,21 @@
+package com.saucelabs.advancedselenium.saucedemo.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    ChromeDriver driver = null;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
