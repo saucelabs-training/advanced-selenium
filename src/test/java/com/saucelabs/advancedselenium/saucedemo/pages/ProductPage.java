b80775b73f825c0dde38f5c2b4837802b43a764a
titusfortner <titusfortner@gmail.com>
2021-04-10 16:40:26 -0500

    move BasePage to resources since it is not implementation specific

--- src/test/java/com/saucelabs/advancedselenium/saucedemo/pages/ProductPage.java
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/pages/ProductPage.java
@@ -2,6 +2,7 @@ package test.java.com.saucelabs.advancedselenium.saucedemo.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import test.java.com.saucelabs.advancedselenium.resources.pages.BasePage;
 
 public class ProductPage extends BasePage {
     private final By addButton = By.className("btn_primary");
