a308ad6bd43d02552fc1d8e644bf2071ddc4ea9e
titusfortner <titusfortner@gmail.com>
2021-04-10 20:42:58 -0500

    implement a User data object

--- src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/AuthenticationTest.java
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/AuthenticationTest.java
@@ -2,32 +2,44 @@ package test.java.com.saucelabs.advancedselenium.saucedemo.tests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import test.java.com.saucelabs.advancedselenium.saucedemo.pages.*;
+import test.java.com.saucelabs.advancedselenium.saucedemo.data.User;
+import test.java.com.saucelabs.advancedselenium.saucedemo.pages.HomePage;
+import test.java.com.saucelabs.advancedselenium.saucedemo.pages.PageFactory;
 
 public class AuthenticationTest extends BaseTest {
     @Test
     public void signInLockedOutUser() {
         HomePage homePage = PageFactory.home(driver);
         homePage.visit();
+        User lockedOutUser = new User();
+        lockedOutUser.setUser("locked_out_user");
+        lockedOutUser.setPassword("secret_sauce");
 
         Assertions.assertDoesNotThrow(() ->
-                homePage.loginLockedOutUserUnsuccessfully("locked_out_user", "secret_sauce"));
+                homePage.loginLockedOutUserUnsuccessfully(lockedOutUser));
     }
 
     @Test
     public void signInSuccessful() {
         HomePage homePage = PageFactory.home(driver);
         homePage.visit();
+        User validUser = new User();
+        validUser.setUser("standard_user");
+        validUser.setPassword("secret_sauce");
 
         Assertions.assertDoesNotThrow(() ->
-                homePage.loginSuccessfully("standard_user", "secret_sauce"));
+                homePage.loginSuccessfully(validUser));
     }
 
     @Test
     public void logout() {
         HomePage homePage = PageFactory.home(driver);
         homePage.visit();
-        homePage.loginSuccessfully("standard_user", "secret_sauce");
+        User validUser = new User();
+        validUser.setUser("standard_user");
+        validUser.setPassword("secret_sauce");
+
+        homePage.loginSuccessfully(validUser);
 
         PageFactory.header(driver).logout();
 
