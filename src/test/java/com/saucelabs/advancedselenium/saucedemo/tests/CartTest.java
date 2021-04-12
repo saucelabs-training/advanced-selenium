c93c73c6c6e2d719daed9598bfd0708bea6e6063
titusfortner <titusfortner@gmail.com>
2021-04-12 12:25:01 -0500

    rewrite tests declaratively

--- src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/CartTest.java
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/CartTest.java
@@ -12,78 +12,57 @@ public class CartTest extends BaseTest {
     @Test
     public void addFromProductPage() {
         HomePage homePage = new HomePage(driver);
-        driver.get(homePage.getUrl());
-        homePage.getUsernameElement().sendKeys("standard_user");
-        homePage.getPasswordElement().sendKeys("secret_sauce");
-        homePage.getSubmitElement().click();
-        InventoryPage inventoryPage = new InventoryPage(driver);
-        inventoryPage.getBoltTShirtLink().click();
+        InventoryPage inventoryPage = homePage.login("standard_user", "secret_sauce");
+        ProductPage productPage = inventoryPage.selectBoltTshirt();
 
-        ProductPage productPage = new ProductPage(driver);
-        productPage.getAddToCartButton().click();
+        productPage.addToCart();
 
-        Assertions.assertEquals("1", productPage.getCartNumberElement().getText());
+        Assertions.assertEquals(1, productPage.cartItems());
     }
 
     @Test
     public void removeFromProductPage() {
         HomePage homePage = new HomePage(driver);
-        driver.get(homePage.getUrl());
-        homePage.getUsernameElement().sendKeys("standard_user");
-        homePage.getPasswordElement().sendKeys("secret_sauce");
-        homePage.getSubmitElement().click();
-        InventoryPage inventoryPage = new InventoryPage(driver);
-        inventoryPage.getBoltTShirtLink().click();
+        InventoryPage inventoryPage = homePage.login("standard_user", "secret_sauce");
+        ProductPage productPage = inventoryPage.selectBoltTshirt();
+        productPage.addToCart();
 
-        ProductPage productPage = new ProductPage(driver);
-        productPage.getAddToCartButton().click();
-        productPage.getRemoveFromCartButton().click();
+        productPage.removeFromCart();
 
-        Assertions.assertTrue(productPage.getCartNumberElements().isEmpty());
+        Assertions.assertEquals(0, productPage.cartItems());
     }
 
     @Test
     public void addFromInventoryPage() {
         HomePage homePage = new HomePage(driver);
-        driver.get(homePage.getUrl());
-        homePage.getUsernameElement().sendKeys("standard_user");
-        homePage.getPasswordElement().sendKeys("secret_sauce");
-        homePage.getSubmitElement().click();
-        InventoryPage inventoryPage = new InventoryPage(driver);
-        inventoryPage.getAddOnesieButton().click();
+        InventoryPage inventoryPage = homePage.login("standard_user", "secret_sauce");
 
-        Assertions.assertEquals("1", inventoryPage.getCartNumberElement().getText());
+        inventoryPage.addOnesieToCart();
+
+        Assertions.assertEquals(1, inventoryPage.cartItems());
     }
 
     @Test
     public void removeFromInventoryPage() {
         HomePage homePage = new HomePage(driver);
-        driver.get(homePage.getUrl());
-        homePage.getUsernameElement().sendKeys("standard_user");
-        homePage.getPasswordElement().sendKeys("secret_sauce");
-        homePage.getSubmitElement().click();
-        InventoryPage inventoryPage = new InventoryPage(driver);
-        inventoryPage.getAddBikeLightButton().click();
-        inventoryPage.getRemoveBikeLightButton().click();
+        InventoryPage inventoryPage = homePage.login("standard_user", "secret_sauce");
 
-        Assertions.assertTrue(inventoryPage.getCartNumberElements().isEmpty());
+        inventoryPage.addBikeLightToCart();
+        inventoryPage.removeBikeLightFromCart();
+
+        Assertions.assertEquals(0, inventoryPage.cartItems());
     }
 
     @Test
     public void removeFromCartPage() {
         HomePage homePage = new HomePage(driver);
-        driver.get(homePage.getUrl());
-        homePage.getUsernameElement().sendKeys("standard_user");
-        homePage.getPasswordElement().sendKeys("secret_sauce");
-        homePage.getSubmitElement().click();
-        InventoryPage inventoryPage = new InventoryPage(driver);
-        inventoryPage.getAddBackpackButton().click();
-        inventoryPage.getAddBikeLightButton().click();
-        inventoryPage.getCartImageLink().click();
+        InventoryPage inventoryPage = homePage.login("standard_user", "secret_sauce");
+        inventoryPage.addBackpackToCart();
+        inventoryPage.addBikeLightToCart();
+        CartPage cartPage = inventoryPage.goToCart();
 
-        CartPage cartPage = new CartPage(driver);
-        cartPage.getRemoveBackPackButton().click();
+        cartPage.removeBackpackFromCart();
 
-        Assertions.assertEquals("1", cartPage.getCartNumberElement().getText());
+        Assertions.assertEquals(1, cartPage.cartItems());
     }
 }
