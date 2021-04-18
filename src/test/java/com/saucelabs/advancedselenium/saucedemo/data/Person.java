0b78963c35a0d65d00a44e89be781bb4c9bb1514
titusfortner <titusfortner@gmail.com>
2021-04-18 15:39:09 -0500

    Exercise Answers: person data implemented

--- src/test/java/com/saucelabs/advancedselenium/saucedemo/data/Person.java
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/data/Person.java
@@ -1,9 +1,18 @@
 package test.java.com.saucelabs.advancedselenium.saucedemo.data;
 
+import com.github.javafaker.Faker;
+
 public class Person {
     private String firstName;
     private String lastName;
     private String postalCode;
+
+    public Person() {
+        Faker faker = new Faker();
+        this.firstName = faker.name().firstName();
+        this.lastName = faker.name().lastName();
+        this.postalCode = faker.address().zipCode();
+    }
 
     public String getFirstName() {
         return firstName;
