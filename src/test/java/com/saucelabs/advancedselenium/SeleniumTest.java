ea33c97a39823c0281fdafe720d8a62cb8728708
titusfortner <titusfortner@gmail.com>
2022-04-20 12:16:32 -0500

    create basic test with JUnit 5

--- src/test/java/com/saucelabs/advancedselenium/SeleniumTest.java
+++ src/test/java/com/saucelabs/advancedselenium/SeleniumTest.java
@@ -1,13 +1,21 @@
 package com.saucelabs.advancedselenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class EnsureWebDriverManagerLoaded {
-    public static void main(String[] args) {
+public class SeleniumTest {
+
+    @Test
+    public void openBrowser() {
+        // Let WebDriverManager handle drivers
         WebDriverManager.chromedriver().setup();
+
+        // Start session (opens browser)
         RemoteWebDriver driver = new ChromeDriver();
+
+        // Quit session (closes browser)
         driver.quit();
     }
 }
