b5f650a61f83c4b8434f01dc9cc7d8f5eb7e9236
titusfortner <titusfortner@gmail.com>
2022-04-20 20:38:11 -0500

    answers: journey split into atomic tests

--- src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/AuthenticationTest.java
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/AuthenticationTest.java
@@ -25,13 +25,43 @@ public class AuthenticationTest {
 
     @Test
     public void signInUnsuccessful() {
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("locked_out_user");
+        driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
+
+        WebElement errorElement = driver.findElement(By.cssSelector("[data-test='error']"));
+        Assertions.assertTrue(errorElement.getText().contains("Sorry, this user has been locked out"),
+                "Error Not Found");
     }
 
     @Test
     public void signInSuccessful() {
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
+
+        Assertions.assertEquals("https://www.saucedemo.com/inventory.html",
+                driver.getCurrentUrl(),
+                "Login Not Successful");
     }
 
     @Test
     public void logout() throws InterruptedException {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("logout_sidebar_link")).click();
+
+        Assertions.assertEquals("https://www.saucedemo.com/",
+                driver.getCurrentUrl(),
+                "Logout Not Successful");
     }
 }
