ee026621443542912bb3b3dc770ff08004477069
titusfortner <titusfortner@gmail.com>
2022-04-22 13:39:07 -0500

    use before hooks

--- src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/CartTest.java
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/CartTest.java
@@ -1,18 +1,21 @@
 package com.saucelabs.advancedselenium.saucedemo.tests;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 public class CartTest extends BaseTest {
-
-    @Test
-    public void addFromProductPage() {
+    @BeforeEach
+    public void login() {
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("standard_user");
         driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
         driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
+    }
 
+    @Test
+    public void addFromProductPage() {
         driver.findElement(By.id("item_1_title_link")).click();
 
         driver.findElement(By.cssSelector("button[data-test='add-to-cart-sauce-labs-bolt-t-shirt']")).click();
@@ -24,10 +27,6 @@ public class CartTest extends BaseTest {
 
     @Test
     public void removeFromProductPage() {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
         driver.findElement(By.id("item_1_title_link")).click();
         driver.findElement(By.cssSelector("button[data-test='add-to-cart-sauce-labs-bolt-t-shirt']")).click();
 
@@ -39,11 +38,6 @@ public class CartTest extends BaseTest {
 
     @Test
     public void addFromInventoryPage() {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
-
         driver.findElement(By.cssSelector("button[data-test='add-to-cart-sauce-labs-onesie']")).click();
 
         Assertions.assertEquals("1",
@@ -52,10 +46,6 @@ public class CartTest extends BaseTest {
 
     @Test
     public void removeFromInventoryPage() {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
         driver.findElement(By.cssSelector("button[data-test='add-to-cart-sauce-labs-bike-light']")).click();
 
         driver.findElement(By.cssSelector("button[data-test='remove-sauce-labs-bike-light']")).click();
@@ -66,10 +56,6 @@ public class CartTest extends BaseTest {
 
     @Test
     public void removeFromCartPage() {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
         driver.findElement(By.cssSelector("button[data-test='add-to-cart-sauce-labs-backpack']")).click();
         driver.findElement(By.className("shopping_cart_link")).click();
 
--- src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/CheckoutTest.java
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/CheckoutTest.java
@@ -1,21 +1,24 @@
 package com.saucelabs.advancedselenium.saucedemo.tests;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 public class CheckoutTest extends BaseTest {
-
-    @Test
-    public void goodInfo() {
+    @BeforeEach
+    public void loginAndGoToCartWithItem() {
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("standard_user");
         driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
         driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
         driver.findElement(By.cssSelector("button[data-test='add-to-cart-sauce-labs-onesie']")).click();
         driver.findElement(By.className("shopping_cart_link")).click();
-        driver.findElement(By.cssSelector("button[data-test='checkout']")).click();
+    }
 
+    @Test
+    public void goodInfo() {
+        driver.findElement(By.cssSelector("button[data-test='checkout']")).click();
         driver.findElement(By.cssSelector("input[data-test='firstName']")).sendKeys("Luke");
         driver.findElement(By.cssSelector("input[data-test='lastName']")).sendKeys("Perry");
         driver.findElement(By.cssSelector("input[data-test='postalCode']")).sendKeys("90210");
@@ -29,12 +32,6 @@ public class CheckoutTest extends BaseTest {
 
     @Test
     public void completeCheckout() {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
-        driver.findElement(By.cssSelector("button[data-test='add-to-cart-sauce-labs-onesie']")).click();
-        driver.findElement(By.className("shopping_cart_link")).click();
         driver.findElement(By.cssSelector("button[data-test='checkout']")).click();
         driver.findElement(By.cssSelector("input[data-test='firstName']")).sendKeys("Luke");
         driver.findElement(By.cssSelector("input[data-test='lastName']")).sendKeys("Perry");
