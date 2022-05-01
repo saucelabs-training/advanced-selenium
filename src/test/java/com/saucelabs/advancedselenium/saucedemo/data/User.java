9571636bfa3d94e58cbee4ee815cd51da0cd0e09
titusfortner <titusfortner@gmail.com>
2022-05-01 15:47:43 -0500

    random user data example

--- src/test/java/com/saucelabs/advancedselenium/saucedemo/data/User.java
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/data/User.java
@@ -1,8 +1,11 @@
 package com.saucelabs.advancedselenium.saucedemo.data;
 
+import net.datafaker.Faker;
+
 public class User {
-    private String username;
-    private String password;
+    private final Faker faker = new Faker();
+    private String username = faker.name().username();
+    private String password = faker.internet().password();
 
     public static User valid() {
         User user = new User();
@@ -16,6 +19,10 @@ public class User {
         user.setUsername("locked_out_user");
         user.setPassword("secret_sauce");
         return user;
+    }
+
+    public static User invalid() {
+        return new User();
     }
 
     public void setUsername(String username) {
