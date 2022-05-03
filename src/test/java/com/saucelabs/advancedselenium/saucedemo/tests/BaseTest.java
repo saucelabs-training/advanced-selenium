ad03524799f9c20cbb78beabef8a9452125f90a5
titusfortner <titusfortner@gmail.com>
2022-05-03 00:03:32 -0500

    create options dynamically from config file

--- src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/BaseTest.java
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package com.saucelabs.advancedselenium.saucedemo.tests;
 
+import com.github.javafaker.shaded.snakeyaml.Yaml;
 import com.saucelabs.advancedselenium.saucedemo.Browser;
 import com.saucelabs.advancedselenium.saucedemo.SauceDemoApp;
 import com.saucelabs.saucebindings.SaucePlatform;
@@ -19,8 +20,14 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 
 public class BaseTest {
@@ -32,8 +39,9 @@ public class BaseTest {
 
     @BeforeAll
     public static void toggleExecution() {
-        // This would normally be toggled via CI tool ENV or similar
+        // These would normally be toggled via CI tool ENV or similar
         System.setProperty("SELENIUM_PLATFORM", "SAUCE");
+        System.setProperty("SAUCE_PLATFORM", "DEFAULT");
     }
 
     @BeforeEach
@@ -53,12 +61,24 @@ public class BaseTest {
     }
 
     private RemoteWebDriver runSauce(TestInfo testinfo) {
-        SauceOptions options = SauceOptions.chrome(getChromeOption())
-                .setName(testinfo.getDisplayName())
-                .setIdleTimeout(Duration.ofSeconds(30))
-                .setUnhandledPromptBehavior(UnhandledPromptBehavior.IGNORE)
-                .setScreenResolution("1280x1024")
-                .build();
+        SauceOptions options;
+
+        String platform = System.getProperty("SAUCE_PLATFORM");
+        if (platform != null && !"DEFAULT".equals(platform)) {
+            options = new SauceOptions();
+            options.sauce().setName(testinfo.getDisplayName());
+            Path path = Paths.get("src/test/java/com/saucelabs/advancedselenium/saucedemo/config.yml");
+            Map<String, Object> capabilities = convertYaml(path, platform);
+            options.mergeCapabilities(capabilities);
+        } else {
+            options = SauceOptions.chrome(getChromeOption())
+                    .setName(testinfo.getDisplayName())
+                    .setIdleTimeout(Duration.ofSeconds(30))
+                    .setUnhandledPromptBehavior(UnhandledPromptBehavior.IGNORE)
+                    .setScreenResolution("1280x1024")
+                    .build();
+        }
+
         session = new SauceSession(options);
         return session.start();
     }
@@ -82,6 +102,19 @@ public class BaseTest {
 
         return chromeOptions;
     }
+
+    private Map<String, Object> convertYaml(Path file, String key) {
+        InputStream input = null;
+        try {
+            input = new FileInputStream(file.toString());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Yaml yaml = new Yaml();
+        Map<String, Object> yamlData = yaml.load(input);
+        return (Map<String, Object>) yamlData.get(key);
+    }
+
 
     public class MyTestWatcher implements TestWatcher {
         @Override
